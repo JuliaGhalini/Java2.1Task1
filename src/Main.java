04c4c7526829e14190cd020626d1d28bafd06435
Ghalini Julia <ghalinijulia@gmail.com>
2020-11-03 19:31:12 +0300

    Add src

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 50_000;
+        int amount = 20;
+        int bonus = ticketPrice / amount ;
+        System.out.println(bonus);
+    }
+}
